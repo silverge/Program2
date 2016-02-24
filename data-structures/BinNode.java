b7c612f253a628f2396de859d66fbff923d35544
silverge <silverg@vt.edu>
2016-02-24 17:28:28 -0500

    Update BinNode.java

--- data-structures/BinNode.java
+++ data-structures/BinNode.java
@@ -1,22 +1,34 @@
+//new stuff
+public interface BinNode {
 
-public class BinNode {
-private Coordinate coordinate;
-private BinNode next;
 
-public BinNode(Coordinate coor, BinNode next){
-    coordinate = coor;
-    this.next = next;
-}
 
-public BinNode(Coordinate coor){
-    coordinate = coor;
-            next = null;
+public boolean isLeaf();
+
+public boolean add(Point coor);
 }
 
-public BinNode getNext() {
-    return next;
-}
-public Coordinate getCoord() {
-    return coordinate;
-}
-}
+
+//old stuff just in case
+// public class BinNode {
+// private Coordinate coordinate;
+// private BinNode next;
+
+// public BinNode(Coordinate coor, BinNode next){
+//     coordinate = coor;
+//     this.next = next;
+// }
+
+// public BinNode(Coordinate coor){
+//     coordinate = coor;
+//             next = null;
+// }
+
+// public BinNode getNext() {
+//     return next;
+// }
+// public Coordinate getCoord() {
+//     return coordinate;
+// }
+// }
+
